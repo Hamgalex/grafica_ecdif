51c5fd2d535a6ada0ae078c8356e5cf89dc672f8
Héctor Alejandro Márquez González <hamgalex@hotmail.com>
2023-01-13 10:32:19 -0600

    se pide la ecuación y la cond inicial

--- app.py
+++ app.py
@@ -2,16 +2,19 @@
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
-  
+
+print("Escriba la ecuación diferencial")
+expression=input()
+
+print("escriba la condicion inicial y0")
+y0=input()
+
 def returns_dydt(y,t):
-    dydt = 6 -t
+    dydt = eval(expression)
     return dydt
   
-# Condición inicial
-y0 = 1
-  
 # Rango de t
-t = np.linspace(-60,80)
+t = np.linspace(0,5)
   
 # resolver la ecdif
 y = odeint(returns_dydt, y0, t)
